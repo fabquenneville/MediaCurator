0a603b222faf579b9eb14a21c9303116418283eb
Fabrice Quenneville <fabquenneville@gmail.com>
2021-04-25 21:43:36 -0400

    Trying to fix docs

--- docsource/makedocs.sh
+++ docsource/makedocs.sh
@@ -4,6 +4,6 @@ find ../docs -mindepth 1 -delete
 find ./build -mindepth 1 -delete
 sphinx-build -b html source build
 cp -r ./build/* ../docs/
-chmod -R 777 ../docs/
 touch ../docs/.nojekyll
+chmod -R 777 ../docs/
 find ./build -mindepth 1 -delete
